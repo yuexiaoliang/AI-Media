8daeec797a36fdd78e6533da60c7ddd9494319f6
岳晓亮 <2220124666@qq.com>
2024-01-02 10:33:13 +0800

    fix(npm-packages): 修复 prompt，避免 yaml 报错

--- index.ts
+++ index.ts
@@ -2,5 +2,5 @@ import 'dotenv/config';
 import { publisher as npmPackagesWeixinPublisher } from '@auto-blog/npm-packages'
 import { publisher as typeChallengesPublisher } from '@auto-blog/type-challenges'
 
-// npmPackagesWeixinPublisher()
-typeChallengesPublisher()
+npmPackagesWeixinPublisher()
+// typeChallengesPublisher()
